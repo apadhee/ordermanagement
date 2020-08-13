78be4304926201fd628c36d59a22ea00095a23d9
Abhilash Padhee <apadhee@deloitte.com>
2020-08-13 13:19:07 +0530

    Code commit

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,7 @@ package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
